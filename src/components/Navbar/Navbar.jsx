d6e0ebba231b9c535dc7520cb4f256ca9df5ebae
Jonnad89 <jonnadvwork@gmail.com>
2022-10-05 13:53:10 -0300

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Navbar.css";
+const Navbar = () => {
+  return (
+    <div className="n-wrapper">
+      <div className="n-left">
+        <div className="n-name">Jonatan</div>
+        <span>toggle</span>
+      </div>
+      <div className="n-right">
+        <div className="n-list">
+          <ul style={{ listStyleType: "none" }}>
+            <li>Home</li>
+            <li>Services</li>
+            <li>Experience</li>
+            <li>Porfolio</li>
+            <li>Testimonials</li>
+          </ul>
+        </div>
+        <button className="button n-button">Contact me</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
