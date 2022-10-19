5f8d4f28b0169cfdd51ad3c26213a0227a42de7f
Jonnad89 <Jonnad89>
2022-10-19 20:17:50 -0300

    add experience

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -4,6 +4,7 @@ import HeartEmoji from "../../img/heartemoji.png";
 import Glasses from "../../img/glasses.png";
 import Humble from "../../img/humble.png";
 import Card from "../Card/Card";
+import Resume from '../Services/Resume.pdf'
 const Services = () => {
   return (
     <div className="services">
@@ -18,8 +19,10 @@ const Services = () => {
           dignissimos beatae aliquam a blanditiis voluptatum, iusto aperiam,
           minus dolor!
         </spane>
+        <a href={Resume} download>
         <button className="button s-button">Download CV</button>
-        <div className="blur s-blur" style={{ background: "#ABF1FF94" }}></div>
+        </a>
+        <div className="blur s-blur1" style={{ background: "#ABF1FF94" }}></div>
       </div>
       {/* right side */}
       <div className="cards">
@@ -30,6 +33,23 @@ const Services = () => {
             detail={"Figma, Sketch, Photoshop, Adobe"}
           />
         </div>
+        {/* Second card */}
+        <div style={{ top: "12rem", left: "-4rem" }}>
+          <Card 
+          emoji={Glasses}
+          heading={"Developer"}
+          detail={"Html, Css, JavaScript, React, NodeJS"}
+          />
+        </div>
+        {/* 3rd */}
+        <div style={{top:"19rem", left:"12rem"}}>
+        <Card  
+        emoji={Humble}
+        heading={"UI/UX"}
+        detail={"Lorem ispum dummy text are usually use in section where"}
+        />
+        </div>
+        <div className="blur s-blur2" style={{background:"var(--purple)"}}></div>
       </div>
     </div>
   );
