e90fe5e6905d0e3b7214536f6b2a7be2c06b2348
Jonnad89 <Jonnad89>
2022-10-20 14:29:37 -0300

    add testimonials

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,9 +1,64 @@
-import './Testimonials.css'
-
+import "./Testimonials.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import ProfilePic1 from "../../img/profile1.jpg";
+import ProfilePic2 from "../../img/profile2.jpg";
+import ProfilePic3 from "../../img/profile3.jpg";
+import ProfilePic4 from "../../img/profile4.jpg";
+import ProfilePic5 from "../../img/profile5.jpg";
 const Testimonials = () => {
-  return (
-    <div>Testimonials</div>
-  )
-}
+  const clients = [
+    {
+      img: ProfilePic1,
+      review:
+        "      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis necessitatibus quam, saepe molestias porro quis perferendis modi dolores soluta, aut pariatur optio dicta illum fugit suscipit? Similique animi asperiores alias!",
+    },
+    {
+      img: ProfilePic2,
+      review:
+        "      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis necessitatibus quam, saepe molestias porro quis perferendis modi dolores soluta, aut pariatur optio dicta illum fugit suscipit? Similique animi asperiores alias!",
+    },
+    {
+      img: ProfilePic3,
+      review:
+        "      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis necessitatibus quam, saepe molestias porro quis perferendis modi dolores soluta, aut pariatur optio dicta illum fugit suscipit? Similique animi asperiores alias!",
+    },
+    {
+      img: ProfilePic4,
+      review:
+        "      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis necessitatibus quam, saepe molestias porro quis perferendis modi dolores soluta, aut pariatur optio dicta illum fugit suscipit? Similique animi asperiores alias!",
+    },
+    {
+      img: ProfilePic5,
+      review:
+        "      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis necessitatibus quam, saepe molestias porro quis perferendis modi dolores soluta, aut pariatur optio dicta illum fugit suscipit? Similique animi asperiores alias!",
+    },
+  ];
 
-export default Testimonials
+  return (
+    <div className="t-wrapper">
+      <div className="t-heading">
+        <span>Clientes always get</span>
+        <span>Exceptional Work</span>
+        <span>from me...</span>
+        <div
+          className="blur t-blur1"
+          style={{ background: "var(--purple" }}
+        ></div>
+        <div className="blur t-blur2" style={{ background: "skyblue" }}></div>
+      </div>
+      {/* slider */}
+      <Swiper>
+        {clients.map((client, index) => {
+          return (
+            <SwiperSlide key={index}>
+              <img src={client.img} alt="" />
+              <span>{client.review}</span>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </div>
+  );
+};
+
+export default Testimonials;
